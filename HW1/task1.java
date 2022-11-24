da78162f804f88caad196c6aa040678fa1825aa3
Dima Tarasov <desant74268@yandex.ru>
2022-11-24 16:47:37 +0300

    HW1 task1

--- HW1/task1.java
+++ HW1/task1.java
@@ -1,5 +1,39 @@
 package HW1;
 
+import java.util.Scanner;
+
 public class task1 {
-    
+    public static void main(String[] args) {
+
+        triangularSolver(inputNum());
+
+    }
+
+    public static int inputNum() {
+        System.out.println("Input number for triangular solver: ");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        System.out.printf("Number: %d\n", num);
+        return num;
+    }
+
+    public static void triangularSolver(int num) {
+        System.out.printf("Sum triangular for number %d = %d\n", num, sumTriangle(num));
+        System.out.printf("Multiply triangular for number %d = %d", num, multTriangle(num));
+    }
+
+    public static int sumTriangle(int num) {
+        if (num == 1) {
+            return 1;
+        } else
+            return (num + sumTriangle(num - 1));
+    }
+
+    public static int multTriangle(int num) {
+        if (num == 1) {
+            return 1;
+        } else
+            return (num * multTriangle(num - 1));
+    }
+
 }
